9e3f2901efa4ec19cd3aff9257e77f3257c0eb0f
praveen <praveenrajjoth@gmail.com>
2019-07-29 07:49:38 +0530

    Task Three completed(Dependencies are injected by using autowire).

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -10,15 +10,11 @@ public class Main
     public static void main( String[] args )
     {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1 = applicationContext.getBean("movie1", Movie.class);
+        Movie movie = applicationContext.getBean("movie", Movie.class);
         Movie Siva = applicationContext.getBean("SivaMovie", Movie.class);
-        Movie SK = applicationContext.getBean("SKMovie", Movie.class);
-        Movie movie2 = applicationContext.getBean("movie2", Movie.class);
 
 
-        System.out.println(movie1);
+        System.out.println(movie);
         System.out.println(Siva);
-        System.out.println(SK);
-        System.out.println(movie1 == movie2);
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -3,14 +3,23 @@ package com.stackroute.domain;
 public class Movie {
     private Actor actor;
 
+    public Movie() {
+
+    }
+
     public Movie(Actor actor) {
         this.actor = actor;
     }
+
 
     @Override
     public String toString() {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
     }
 }
